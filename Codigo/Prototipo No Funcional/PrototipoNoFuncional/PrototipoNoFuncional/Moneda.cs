6731712791317b7e7d3caea91db3f263c71c506a
LuisTorres2122 <ltorresb@miumg.edu.gt>
2023-04-15 13:52:29 -0600

    Proyecto Independiente de Prototipo No funcional

---
+++ Codigo/Prototipo No Funcional/PrototipoNoFuncional/PrototipoNoFuncional/Moneda.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrototipoNoFuncional
+{
+    public partial class Moneda : Form
+    {
+        public Moneda()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
